111e0a09c4f2f143d8a1b12aca38ad5ff6bef218
Beginner090902 <milibo45@gmail.com>
2026-05-03 20:30:31 +0200

    change face detektion and db handeleing with olready scand fieles

--- b_gesicht_erkennung.py
+++ b_gesicht_erkennung.py
@@ -1,24 +1,25 @@
 from peewee import *
 import cv2
 from insightface.app import FaceAnalysis
-from src.a_db_ordner_handler import Bilder_daten_Handler
 from src.g_db_settings_handler import SettingsHandler
 from src.custom_logging import setup_logger
 from PySide6.QtCore import Qt  
 from PySide6.QtGui import QPixmap, QImage
 from PySide6.QtWidgets import QApplication
 import cv2
+from src.DBManager import FaceDB
 import os
 
 loger = setup_logger(__name__)
 
 def starte_gesicht_erkennung_alle(ui):
+    clear_images(ui)
     folder_path = ui.selected_folder_path.text()
     db_path = f"{folder_path}/db.db"
 
-    bilder_db = Bilder_daten_Handler(db_path=db_path)
+    bilder_db = FaceDB(db_path=db_path)
     settings_db = SettingsHandler(db_path=db_path)
-    alle_bilder_ojekte = bilder_db.get_all_bilder()
+    alle_bilder_ojekte = bilder_db.get_all_images()
 
     # ProgressBar konfigurieren
     ui.scan_progressBar.setMaximum(len(alle_bilder_ojekte))
@@ -27,48 +28,63 @@ def starte_gesicht_erkennung_alle(ui):
     ui.scan_progressBar.setVisible(True)
     ui.scan_progressBar.setFormat("%p% - %v von %m Bildern")
 
+    for i, bilder in enumerate(alle_bilder_ojekte):
     # 1. ORIGINALBILD path
-    bild_path = alle_bilder_ojekte[0].name
-    voller_pfad = f"{folder_path}/{bild_path}"
-    
-
-    pixmap = QPixmap(voller_pfad)
-    scalier_und_anzeigen_in_objekt(element=ui.label,pixmap=pixmap)
-
-
-    # 2. Modell laden für Gesichtserkennung
-    app = FaceAnalysis(name='antelopev2')
-    if settings_db.mode == "CPU":
-        mode = -1
-    elif settings_db.mode == "GPU":  # elif statt if
-        mode = 0
-    else:
-        loger.error(f"unplausible einstellung CPU oder GPU modus aus DB: {settings_db.mode}")
-        return
-    
-    app.prepare(ctx_id=mode)
-
-    # 3. Bild mit OpenCV laden und Gesichter erkennen
-    img = cv2.imread(voller_pfad)
-    faces = app.get(img)
-
-    # 4. Rechtecke auf das Bild zeichnen
-    for face in faces:
-        print("Embedding Länge:", len(face.embedding))
-        print("Bounding Box:", face.bbox)
+        faces_by_image = bilder_db.get_faces_by_image(bilder.file_name)
+        if faces_by_image:
+            progress_value = i + 1
+            ui.scan_progressBar.setValue(progress_value)
+            continue
+        bild_path = bilder.file_name
+        voller_pfad = f"{folder_path}/{bild_path}"
         
-        x1, y1, x2, y2 = map(int, face.bbox)
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 8)
 
-    # Konvertiere OpenCV-Bild (BGR) zu QPixmap
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    h, w, ch = img_rgb.shape
-    bytes_per_line = ch * w
-    qt_image = QImage(img_rgb.data, w, h, bytes_per_line, QImage.Format_RGB888)
-    modifizierte_pixmap = QPixmap.fromImage(qt_image)
-    scalier_und_anzeigen_in_objekt(element=ui.label_2,pixmap=modifizierte_pixmap)
+        pixmap = QPixmap(voller_pfad)
+        scalier_und_anzeigen_in_objekt(element=ui.label,pixmap=pixmap)
 
-    loger.info(f"Bild mit {len(faces)} Gesichtern erkannt")
+
+        # 2. Modell laden für Gesichtserkennung
+        app = FaceAnalysis(name='antelopev2')
+        if settings_db.mode == "CPU":
+            mode = -1
+        elif settings_db.mode == "GPU":  # elif statt if
+            mode = 0
+        else:
+            loger.error(f"unplausible einstellung CPU oder GPU modus aus DB: {settings_db.mode}")
+            return
+        
+        app.prepare(ctx_id=mode)
+
+        # 3. Bild mit OpenCV laden und Gesichter erkennen
+        img = cv2.imread(voller_pfad)
+        faces = app.get(img)
+
+        # 4. Rechtecke auf das Bild zeichnen
+        faces_list = []
+        for face in faces:
+            faces_list.append({
+                'embedding': face.embedding,  # numpy array
+                'bbox': list(map(int, face.bbox)),  # [x1,y1,x2,y2]
+                'age': None,  # optional
+                'gender': None  # optional
+            })
+            
+            x1, y1, x2, y2 = map(int, face.bbox)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 8)
+        face_ids = bilder_db.add_faces(bilder, faces_list)
+
+        # Konvertiere OpenCV-Bild (BGR) zu QPixmap
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        h, w, ch = img_rgb.shape
+        bytes_per_line = ch * w
+        qt_image = QImage(img_rgb.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        modifizierte_pixmap = QPixmap.fromImage(qt_image)
+        scalier_und_anzeigen_in_objekt(element=ui.label_2,pixmap=modifizierte_pixmap)
+
+        loger.info(f"Bild mit {face_ids} Gesichtern erkannt")
+        progress_value = i + 1
+        
+        ui.scan_progressBar.setValue(progress_value)
     
 
 def scalier_und_anzeigen_in_objekt(element,pixmap):
@@ -83,3 +99,7 @@ def scalier_und_anzeigen_in_objekt(element,pixmap):
     element.setPixmap(scaled_pixmap)
     element.setAlignment(Qt.AlignmentFlag.AlignCenter)
     QApplication.processEvents()  # Qt aktualisieren
+
+def clear_images(ui):
+    ui.label.clear()
+    ui.label_2.clear()
