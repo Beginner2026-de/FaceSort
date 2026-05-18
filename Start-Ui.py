8961bb28ef623f925c04904468dec7bcdccf870c
Beginner090902 <milibo45@gmail.com>
2026-05-18 03:21:39 +0200

    add merge funktion

--- Start-Ui.py
+++ Start-Ui.py
@@ -10,7 +10,7 @@ from src.g_db_settings_handler import SettingsHandler
 from src.resource_path import resource_path
 from main_tab.c_unter_tab.start_personen_scan import start_personen_scan, person_nachrichten_handler
 from main_tab.c_unter_tab.benenen import start_personen_bennen, benenen_personen_name_abschiekcen_button
-from main_tab.c_unter_tab.zusammenfuegen import start_personen_zusammenfuegen
+from main_tab.c_unter_tab.zusammenfuegen import start_personen_zusammenfuegen, zusammenfuegen_button_gedrückt
 
 loger = setup_logger(__name__)
 
@@ -43,6 +43,9 @@ class MainWindow(QMainWindow):
         #Personen unbennen
         self.ui.personen_tabWidget.currentChanged.connect(lambda: on_chnage_in_person_tab(ui=self.ui))
         self.ui.benenen_personen_name_abschiekcen_button.clicked.connect(lambda: benenen_personen_name_abschiekcen_button(ui=self.ui))
+        #Personen zusammenfuegen
+        self.ui.zusammenfuegen_button.clicked.connect(lambda: zusammenfuegen_button_gedrückt(ui=self.ui))
+
 
         def on_chnage_in_person_tab(ui):
             index = ui.personen_tabWidget.currentIndex()
