8a354b900d2a1d8ee1f57c238306ecb4431cbc54
Beginner090902 <milibo45@gmail.com>
2026-05-22 20:12:56 +0200

    change funktion

--- src/resource_path.py
+++ src/resource_path.py
@@ -2,8 +2,12 @@ import sys
 import os
 
 def resource_path(relative_path):
-    if getattr(sys, 'frozen', False):
+    """ Ermittelt den Pfad zur UI-Datei, passend für Entwicklung und Nuitka-Standalone """
+    if hasattr(sys, "_MEIPASS"): # Falls du mal PyInstaller testen solltest
         base_path = sys._MEIPASS
     else:
-        base_path = os.path.abspath(".")
+        # Bei Nuitka zeigt das Verzeichnis von sys.argv[0] im Standalone-Modus 
+        # immer direkt in den .dist-Ordner, wo auch dein 'QT-Ui'-Ordner landen wird.
+        base_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+        
     return os.path.join(base_path, relative_path)
